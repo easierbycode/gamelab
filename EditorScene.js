5664d7f2e5e43d73f69b0c01d152537347182549
▓▒░ ♔ Daniel ♔ ░▒▓ <563975+easierbycode@users.noreply.github.com>
2026-03-02 03:56:38 -0700

    Change Example3 import to external URL

--- EditorScene.js
+++ EditorScene.js
@@ -9,7 +9,7 @@ import LevelEditorScene from './LevelEditorScene.js';
 import BossViewerScene from './bosses.js'; // Import the new Boss Viewer Scene
 import Example from './specs/Example.js'; // Import Example scene
 import Example2 from './specs/Example2.js'; // Import Example2 scene
-import Example3 from './specs/Example3.js'; // Import Example3 scene
+import Example3 from 'https://easierbycode.com/games/evil-invaders/objects-and-scenes.js'; // Import Example3 scene
 import GameScene from './specs/Example4.js'; // Import MyScene
 
 
